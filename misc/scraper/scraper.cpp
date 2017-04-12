8326742d6f962a1afadf4332918dc58e56f886fb
alufers <alufers@wp.pl>
2017-04-12 13:21:24 +0200

    Added SDSTOS

---
+++ misc/scraper/scraper.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  int arr[1];
+  long i;
+  while(true) {
+      cout << arr[i];
+      i++;
+  }
+  return 0;
+}
---
+++ zajecia/SDSTOS/SDSTOS.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  vector<int> vec;
+  int liczbaOperacji = 0;
+  cin >> liczbaOperacji;
+  for (int i = 0; i < liczbaOperacji; i++) {
+    string op;
+    cin >> op;
+    if (op == "push") {
+      int tmp;
+      cin >> tmp;
+      vec.push_back(tmp);
+    } else if (op == "top") {
+      if (vec.size() == 0) {
+        continue;
+      }
+      cout << vec[vec.size() - 1] << "\n";
+    } else if (op == "pop") {
+      if (vec.size() == 0) {
+        continue;
+      }
+      vec.pop_back();
+    }
+  }
+  cout << "all done\n";
+  return 0;
+}
